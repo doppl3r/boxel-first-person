8e470e829a20587267d53048a5d5f9f31882b42e
Jacob DeBenedetto <deben3@gmail.com>
2023-03-03 14:34:51 -0700

    Add electron support

---
+++ src/ts/main.ts
@@ -0,0 +1,9 @@
+import { createApp } from 'vue'
+import '../scss/style.scss'
+import App from '../vue/App.vue'
+
+createApp(App)
+  .mount('#app')
+  .$nextTick(() => {
+    postMessage({ payload: 'removeLoading' }, '*')
+  })
---
+++ src/vue/App.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts">
+  import Menu from './Menu.vue';
+  import '../js/app.js';
+</script>
+
+<template>
+  <Menu msg="Test" />
+</template>
